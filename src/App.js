988eb7dd7bc205ac0c20ead0e80598a45704698d
nmariahpaul <npaul@mtroyal.ca>
2022-11-28 17:18:26 -0700

    Created file structure
    Created homepage component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import "./MealPlanHomepage";
+import MealPlanHomepage from "./MealPlanHomepage";
+
+export default function App() {
+  return (
+    <div className="App">
+      <MealPlanHomepage />
+    </div>
+  );
+}
---
+++ src/MealPlanHomepage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MealPlanHomepage() {
+  return <div className="MealPlanHomepage">Hello from homepage</div>;
+}
