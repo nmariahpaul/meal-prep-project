76ddaee61dac05af9024b054d3309537a011d476
nmariahpaul <npaul@mtroyal.ca>
2022-12-01 17:15:26 -0700

    Updated h1

--- src/MealPlanHomepage.js
+++ src/MealPlanHomepage.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default function MealPlanHomepage() {
   return (
     <div className="MealPlanHomepage">
-      <h1>Hello from homepage</h1>
+      <h1>Welcome to Mariah's Meal Guide!</h1>
     </div>
   );
 }
